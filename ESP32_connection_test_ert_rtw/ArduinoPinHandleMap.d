5d66ad8e9597d84cef79ca022f219aad271dd55e
Joseph <jh.delucas@gmail.com>
2026-05-22 12:28:47 -0300

    remove modelo  e arquivos associados

--- ESP32_connection_test_ert_rtw/ArduinoPinHandleMap.d
+++
@@ -1,4 +0,0 @@
-ArduinoPinHandleMap.o: \
- C:/ProgramData/MATLAB/SupportPackages/R2026a/toolbox/target/supportpackages/arduinobase/src/ArduinoPinHandleMap.cpp \
- C:/Users/jhdel/CDig_Prj1/CDig_Prj/ESP32_connection_test_ert_rtw/rtwtypes.h \
- C:/PROGRA~1/MATLAB/R2026a/extern/include/tmwtypes.h
--- ESP32_connection_test_ert_rtw/MW_PWMDriver.d
+++
@@ -1,5 +0,0 @@
-MW_PWMDriver.o: \
- C:/ProgramData/MATLAB/SupportPackages/R2026a/toolbox/target/supportpackages/arduinobase/src/MW_PWMDriver.c \
- C:/ProgramData/MATLAB/SupportPackages/R2026a/toolbox/target/supportpackages/arduinobase/include/MW_PWMDriver.h \
- C:/Users/jhdel/CDig_Prj1/CDig_Prj/ESP32_connection_test_ert_rtw/rtwtypes.h \
- C:/PROGRA~1/MATLAB/R2026a/extern/include/tmwtypes.h
